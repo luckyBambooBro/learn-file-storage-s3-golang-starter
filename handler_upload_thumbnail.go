9ba3d43709ad7f58d2e9be021a4ded1ba79b2508
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-06-17 16:25:02 +0930

    finished 1.5 check solutions and make sure i understand everything before moving on

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -32,6 +33,47 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	//dunno why lesson doesnt error check the following, but ill follow suit for now
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to parse multipart form", err)
+	}
+	defer file.Close()
+	mediaType := header.Header.Get("Content-Type")
+	imgData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to read file", err)
+		return
+	}
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "video with provided uuid does not exist", err)
+		return
+	}
+	//unauthorised access if logged in user is not owner of the video
+	if videoMetadata.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorised access. current user is not user that owns video", nil)
+		return
+	}
+
+	videoThumbnail := thumbnail{
+		data: imgData,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = videoThumbnail
+
+	//update video url
+	thumbnailURL := "http://localhost:" + cfg.port + "/api/thumbnails/" + videoID.String()
+	videoMetadata.ThumbnailURL = &thumbnailURL
+	if err = cfg.db.UpdateVideo(videoMetadata); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to update database with video metadata", err)
+		return
+	}
+
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
