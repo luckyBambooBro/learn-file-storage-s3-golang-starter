e03c8d69bf5e3615d19296086d03e561029836f5
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-06-25 23:50:38 +0930

    up to q3.5

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,46 @@ package main
 
 import (
 	"net/http"
+
+	"internal/auth"
+
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	//Set an upload limit of 1 GB (1 << 30 bytes) using http.MaxBytesReader.
+	r.Body = http.MaxBytesReader(w, r.Body, 10 << 30)
+	
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID) 
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to retrieve video metadata", err)
+	}
+	if userID != video.UserID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorised user access for requested video", nil)
+	}
+
+	
+
+	
+
+
+}
