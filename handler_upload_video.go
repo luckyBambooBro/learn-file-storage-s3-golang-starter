ddeb1a5c4349465f0bb555a54008bd3048dd0caf
Huy Pham <huypham@Adas-MacBook-Pro.local>
2026-06-27 16:50:24 +0930

    up to q7

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"mime"
 	"net/http"
 
 	"internal/auth"
@@ -10,7 +11,7 @@ import (
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 	//Set an upload limit of 1 GB (1 << 30 bytes) using http.MaxBytesReader.
-	r.Body = http.MaxBytesReader(w, r.Body, 10 << 30)
+	r.Body = http.MaxBytesReader(w, r.Body, 1 << 30)
 	
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
@@ -34,17 +35,36 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	video, err := cfg.db.GetVideo(videoID) 
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "unable to retrieve video metadata", err)
+		return
 	}
 	if userID != video.UserID {
 		respondWithError(w, http.StatusUnauthorized, "unauthorised user access for requested video", nil)
+		return
 	}
 
 	const maxMemory = 32 << 20
-	r.ParseMultipartForm(maxMemory)
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		respondWithError(w, http.StatusBadRequest, "failed to parse form", err)
+		return
+	}
 
-	file, header, err := r.Formfile()
-
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "failed to parse form", err)
+		return
+	}
+	defer file.Close()
 	
+	mediatype, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse media type", err)
+		return
+	}
+	if mediatype != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "must upload video/mp4 file", nil)
+		return
+	}
+
 
 
 }
